9ca826dc56c1c6c675420eafbb3a1aebde886400
Anhhieu2702 <vungochieu0999@gmail.com>
2024-12-21 14:56:16 +0700

    Create models and define properties

---
+++ app/models/Order.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Order extends Model
+{
+    public $timestamps = false;
+   protected $fillable = [ 'customer_id', 'order_date', 'status'];
+}
