c1601995cb5ce21e503feb1608bc7e761f58774c
Anhhieu2702 <vungochieu0999@gmail.com>
2024-12-22 15:57:53 +0700

    edit link to order_app

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Task List App</title>
+    <title>Hiệu thuốc</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
 </head>
 <body>
@@ -22,6 +22,10 @@
                     <li class="nav-item">
                         <a class="nav-link active" aria-current="page" href="#">Quản lý sản phẩm</a>
                     </li>
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="{{ route('orders.index') }}">Quản lý hàng chờ</a>
+                    </li>
+
                     <li class="nav-item">
                         <a class="nav-link active" aria-current="page" href="#">Quản lý đơn hàng</a>
                     </li>
