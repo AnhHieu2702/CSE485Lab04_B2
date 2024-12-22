82640d64b49076b2007f3cfa3472e41cbe59afb2
Vuongdao21 <vuongds2111@gmail.com>
2024-12-22 21:11:21 +0700

    declare OrderDetailController resource

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\OrderDetailController;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::resource('order_details', OrderDetailController::class);
